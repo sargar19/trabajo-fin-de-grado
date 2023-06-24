e31918dd4a617a456ecaf71c0e3c54714276048b
sara <sargar19@ucm.es>
2023-06-24 10:04:30 +0200

    modified build_sample.py to build_sample_hdfs.py and merged with main.py to work on hadoop file system files - Sara

--- macros/build_sample_hdfs.py
+++ macros/build_sample_hdfs.py
@@ -48,29 +48,58 @@ def sample_fichero(filename_entrada, filename_salida, tamaño, header):
     fs.delete(sc._jvm.org.apache.hadoop.fs.Path(TMP_OUTPUT_DIR), True)
     print(f'Sample file {filename_spark} renamed to desired filename: {filename_salida}')
     sc.stop()
-    
+
+def main(filename, tamaños, samples_number):
+    #1. Build sample files from filename
+    print('--------------------- Building samples ---------------------')
+    filenames = []
+    tamaños = [float(x) for x in tamaños.strip('][').split(',')]
+    try:
+        for sample_number in range(int(samples_number)):
+            for tamaño in tamaños:
+                filename_prefix = filename[:filename.find('.')]
+                sample_filename = filename_prefix + '_' + '_sample_' + str(tamaño).zfill(3).replace('.','') + '_' + str(sample_number+1) + '.txt'
+                sample_fichero(filename, sample_filename, tamaño, 0)
+                filenames = filenames + [sample_filename]
+        print('-------------------- Samples built -------------------------')
+        print(filenames)
+        """try:
+            for filename in filenames:
+                app_name = 'app_narrow_transf_' + sample_filename
+                conf_parameters = [app_name] + conf_parameters
+                narrow_transformations(conf_parameters, filename, filename_desc)
+        except:
+            raise"""
+    except:
+        print('------------------------- Error ---------------------------')
+        print(f'Unable to build samples for file {filename}')
+        raise
+
 if __name__ == "__main__":
     try:
-        assert len(sys.argv) == 5
-        sample_fichero(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-        print(f'Creación del fichero de tamaño reducido {sys.argv[2]} satisfactoria.')
+        assert len(sys.argv) == 4
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
+        print(f'Creación del fichero de tamaño reducido {sys.argv[2]} satisfactoria.')   
     except AssertionError:
         print('------------------------------------- ERROR ----------------------------------------')
         print('Ejecuta el siguiente comando:')
         print('')
-        print('    python3 fichero_reducido.py <filename_entrada> <filename_salida> <tamaño> <header>')
+        print('    python3 build_sample_hdfs.py <filename_entrada> <filename_salida> <tamaño> <header>')
         print('')
-        print('   1. <filename_entrada> [str]: numbre del fichero para samplear (localizado en hdfs user/sargar19/input/)')
+        print('   1. <filename> [str]: numbre del fichero para samplear (localizado en hdfs user/sargar19/input/)')
         print('')
-        print('   2. <filename_salida> [str]: nombre deseado para el fichero sampleado')
+        print('   2. <tamaños> [lst]: list de valores xi (float) entre 0 y 1 tales que size(sample_file) = xi * 100 % size(filename)')
+        print('         Ejemplo: [0.01,0.1] --> construirá 2 samples con 1% y 10%, respectivamente, del tamaño del fichero original')
         print('')
-        print('   3. <tamaño> [str]: nombre del fichero con las descripciones de los campos del fichero <filename_entrada>')
-        print('')
-        print('   4. <header> [bool]: 1 si el fichero contiene como primera línea los nombres de los campos del fichero - 0 en caso contrario')
+        print('   3. <samples_number> [int]: número de ficheros samples de cada uno de los tamaños anteriores.')
         print('')
         print('-----------------------------------------------------------------------------------')
+    except Exception:
+        print('')
+        raise
     except FileNotFoundError:
         print('------------------------------------- ERROR ----------------------------------------')
         print(f'No se encuentra el fichero {sys.argv[1]}. Por favor, comprueba la existencia de este.')
         print('------------------------------------------------------------------------------------')
         raise
+
